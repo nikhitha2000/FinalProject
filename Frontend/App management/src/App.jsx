f061cca815cc9fb7b5b5675e5c77993a8fa0eae4
Nikhitha <nikhibeeraka2000@gmail.com>
2024-10-21 18:52:38 +0530

    Working on Registration and Login page

---
+++ Frontend/App management/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './Components/Register';
+import Login  from './Components/Login';
+
+function App() {
+  return (
+    <div>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/App management/src/Components/Register.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import styles from '../Components/Register.module.css';
+import robo from '../assets/Group.png';
+import icon1 from '../assets/Frame 1036.png';
+import icon2 from '../assets/icon.png';
+import icon3 from '../assets/lock.png';
+import icon4 from '../assets/view.png';
+import icon5 from '../assets/Vector.png';
+function Register() {
+    const[ShowPassword,setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const[CurrentEyeIcon, setCurrentEyeIcon] = useState(icon4);
+    const [currentConfirmEyeIcon, setCurrentConfirmEyeIcon] = useState(icon4);
+ const handleLoginClick = () => {
+    window.location.href = '/login';
+};
+const ToggleEyeIcon = () =>{
+    setShowPassword(!ShowPassword);
+    setCurrentEyeIcon(ShowPassword ? icon4 : icon5 );
+}
+const ToggleConfirmEyeIcon = ()=>{
+    setShowConfirmPassword(!showConfirmPassword);
+    setCurrentConfirmEyeIcon(showConfirmPassword? icon4 : icon5);
+}
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}>
+            <div className={styles.roboContainer}>
+            <div className={styles.backgroundCircle}></div>
+           <img className={styles.robo} src={robo} alt="Robot Illustration" />
+           </div>
+            <p className={styles.para}>Welcome aboard my friend</p>
+            <span className={styles.span}>just a couple of clicks and we start</span>
+        </div>
+        < div className={styles.Right}>
+            <h3>Register</h3>
+            <div className={styles.inputGroup}>
+            <img src={icon1} alt="Name Icon" className={styles.icon} />
+            <input type='text' placeholder="Name" className={styles.inputField} />
+            </div>
+            <div className={styles.inputGroup}>
+            <img src = {icon2} alt="Email" className={styles.icon} />
+            <input type = 'email' placeholder='Email' className={styles.inputField} />
+            </div>
+            <div className={styles.inputGroup}>
+            <img src = {icon3} alt= "Confirm Password" className={styles.icon} />
+            <input type = 'password' placeholder='Confirm Password' className={styles.inputField} />
+            <img src = {currentConfirmEyeIcon} alt="Show Password" className={styles.eyeIcon}  onClick={ToggleConfirmEyeIcon} />
+            </div>
+            <div className={styles.inputGroup}>
+            <img src={icon3} alt= "Password" className={styles.icon} />
+            <input type='password' placeholder='Password' className={styles.inputField} />
+            <img src = {CurrentEyeIcon} alt="Show Password" className={styles.eyeIcon} onClick={ToggleEyeIcon}/>
+
+            </div>
+            <button className={styles.registerButton}>Register</button>
+            <div className={styles.loginLink}>
+            <p>Have an account?</p>
+            <button className={styles.LoginButton} onClick={handleLoginClick}>Login</button>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
