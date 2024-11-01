1ac5ba1f84c1ed78b7e54fe149051aa9f73d11a5
Nikhitha <nikhibeeraka2000@gmail.com>
2024-11-01 18:42:01 +0530

    solving bugs

--- Server/controllers/Taskcontroller.js
+++ Server/controllers/Taskcontroller.js
@@ -2,20 +2,16 @@ const Task = require('../models/Taskmodel');
 
 // Function to create a new task
 exports.createTask = async (req, res) => {
-    const { title, priority, checklist ,dueDate,assignedEmails  } = req.body;
-    if (dueDate !== "" && dueDate !== null && isNaN(new Date(dueDate).getTime())) {
-      return res.status(400).json({ message: "Invalid due date" });
-    }
-
-    try {
-      const newTask = new Task({ title, priority, checklist , dueDate: dueDate ? new Date(dueDate) : null,assignedEmails });
+  try {
+      const newTask = new Task(req.body);
       const savedTask = await newTask.save();
       res.status(201).json(savedTask);
-    } catch (error) {
-        console.error('Error creating task:', error); 
-      res.status(500).json({ message: 'Error creating task' });
-    }
-  };
+  } catch (error) {
+      console.error("Error saving task:", error);
+      res.status(500).json({ message: "Error saving task" });
+  }
+};
+
 
 // Function to get all tasks
 exports.getTasks = async (req, res) => {
@@ -28,24 +24,29 @@ exports.getTasks = async (req, res) => {
 };
 
 exports.updateTask = async (req, res) => {
+  const { taskId } = req.params;
+  const updatedData = req.body;
+
   try {
-      const taskId = req.params.id;
-      const updatedTaskData = req.body;
-      console.log("Task ID:", taskId);
-      console.log("Update Data:", updatedTaskData);
-      const updatedTask = await Task.findByIdAndUpdate(
-          taskId,
-          updatedTaskData,
-          { new: true }
-      );
-      console.log("Updated Task:", updatedTask);
-      if (!updatedTask) {
+    const updatedTask = await Task.findByIdAndUpdate(taskId, updatedData, { new: true });
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ data: updatedTask });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating task" });
+  }
+};
+
+exports.deletedTask=  async (req, res) => {
+  const { id } = req.params;
+  try {
+      const deletedTask = await Task.findByIdAndDelete(id);
+      if (!deletedTask) {
           return res.status(404).json({ message: "Task not found" });
       }
-
-      res.json(updatedTask);
+      res.status(200).json({ message: "Task deleted successfully" });
   } catch (error) {
-      console.error("Error updating task:", error);
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: "Error deleting task" });
   }
 };
