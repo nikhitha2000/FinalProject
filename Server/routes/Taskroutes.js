42c0f8918076a340ee1648738624be96bb0a5b53
Nikhitha <nikhibeeraka2000@gmail.com>
2024-11-01 18:39:50 +0530

    solving bugs

--- Server/routes/Taskroutes.js
+++ Server/routes/Taskroutes.js
@@ -3,10 +3,9 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/Taskcontroller');
 
-// Route to fetch tasks with filtering
 router.post('/', taskController.createTask);
 router.get('/', taskController.getTasks);
-router.put('/taskId', taskController.updateTask);
+router.put('/:id', taskController.updateTask);
 router.delete('/:id', taskController.deletedTask);
 
 module.exports = router;
